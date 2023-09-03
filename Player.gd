d1b6a7eb4a1676e6a9812aeb06d0fd499e67c35c
Zettexe <zettexe@mailbox.org>
2023-09-03 15:15:49 +0200

    Added interaction functionality

--- Player.gd
+++ Player.gd
@@ -3,6 +3,21 @@ extends CharacterBody2D
 const SPEED = 500
 const STOPPING_FACTOR = 20
 
+var overlapping_areas = []
+
+func _input(event):
+	if event.is_action("interact", true) and event.pressed and overlapping_areas.size() > 0:
+		var shortest_distance_area = overlapping_areas[0]
+		for area in overlapping_areas:
+			if area == shortest_distance_area:
+				continue
+			if global_position.distance_squared_to(area.global_position) < \
+			global_position.distance_squared_to(shortest_distance_area.global_position):
+				shortest_distance_area = area
+		var node = shortest_distance_area.get_parent()
+		if node.has_method("_interact"):
+			node._interact()
+
 func _physics_process(delta):
 	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	if direction:
@@ -11,3 +26,9 @@ func _physics_process(delta):
 		velocity = velocity.move_toward(Vector2.ZERO, delta * SPEED * STOPPING_FACTOR)
 	
 	move_and_slide()
+
+func _on_player_area_entered(area):
+	overlapping_areas.append(area)
+
+func _on_player_area_exited(area):
+	overlapping_areas.erase(area)
---
+++ TestNegotiator.gd
@@ -0,0 +1,4 @@
+extends StaticBody2D
+
+func _interact():
+	print("Interacted with TestNegotiator")
