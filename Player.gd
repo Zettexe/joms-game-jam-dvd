c14617e9038cbb099d88f4cd4f60ca6eefb06f9f
Zettexe <zettexe@mailbox.org>
2023-09-03 16:16:51 +0200

    Add more interaction code

--- Player.gd
+++ Player.gd
@@ -2,6 +2,7 @@ extends CharacterBody2D
 
 const SPEED = 500
 const STOPPING_FACTOR = 20
+var lock_velocity = false
 
 var overlapping_areas = []
 
@@ -16,11 +17,11 @@ func _input(event):
 				shortest_distance_area = area
 		var node = shortest_distance_area.get_parent()
 		if node.has_method("_interact"):
-			node._interact()
+			node._interact(self)
 
 func _physics_process(delta):
 	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
-	if direction:
+	if direction and not lock_velocity:
 		velocity = direction * SPEED
 	else:
 		velocity = velocity.move_toward(Vector2.ZERO, delta * SPEED * STOPPING_FACTOR)
@@ -32,3 +33,9 @@ func _on_player_area_entered(area):
 
 func _on_player_area_exited(area):
 	overlapping_areas.erase(area)
+
+func lock_movement():
+	lock_velocity = true
+
+func unlock_movement():
+	lock_velocity = false
--- TestNegotiator.gd
+++ TestNegotiator.gd
@@ -1,4 +1,24 @@
 extends StaticBody2D
 
-func _interact():
+signal interaction_done
+var interacting = false
+
+func _interact(player):
 	print("Interacted with TestNegotiator")
+	player.lock_movement()
+	
+	interacting = true
+	await interaction_done
+	interacting = false
+	
+	player.unlock_movement()
+
+var timer = 0
+func _process(delta):
+	if not interacting:
+		return
+	
+	print(timer)
+	timer += 1
+	if timer >= 500:
+		emit_signal("interaction_done")
