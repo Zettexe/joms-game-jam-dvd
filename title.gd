8959fb886e7eb3fed4149e24b6080b2809846536
CMRicketts <>
2023-09-07 13:24:07 -0400

    finished title screen

--- title.gd
+++ title.gd
@@ -2,11 +2,27 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	print("hit title screen!")
-	$AnimationPlayer.play("title_screen")
+	$AnimationPlayer.play("studio_fade")
 	await get_tree().create_timer(5).timeout
-	$AnimationPlayer.play_backwards("title_screen")
+	$AnimationPlayer.play_backwards("studio_fade")
 	await get_tree().create_timer(3).timeout
+	
+	$ctrlz.position = Vector2(1800, 0)
+	$AnimationPlayer.play("studio_fade")
+	await get_tree().create_timer(3).timeout
+	$AnimationPlayer.play_backwards("studio_fade")
+	await get_tree().create_timer(3).timeout
+	
+	$title_screen.position = Vector2(1800, 0)
+	$AnimationPlayer.play("studio_fade")
+	await get_tree().create_timer(2).timeout
+	
+	$AnimationPlayer.play("one_to_two_text")
+	await get_tree().create_timer(20).timeout
+	$AnimationPlayer.play_backwards("studio_fade")
+	
+	await get_tree().create_timer(3).timeout
+	
 	get_tree().change_scene_to_file("res://testscene.tscn")
 
 
